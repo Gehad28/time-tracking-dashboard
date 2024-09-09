e7524414e677ab013d9903be8239e2d327e1e5f1
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-09-09 14:26:40 +0300

    Desktop View

---
+++ script.js
@@ -0,0 +1,53 @@
+let container = document.getElementById("container");
+
+let titles = document.querySelectorAll(".title");
+let currentTimes = document.querySelectorAll(".current");
+let previousTimes = document.querySelectorAll(".previous");
+let listItems = document.querySelectorAll("li");
+
+let data = [];
+
+const perviousText = {
+    "daily": "Yasterday",
+    "weekly": "Last Week",
+    "monthly": "Last Month"
+}
+
+fetch("/data.json").then((request) => {
+    if(!request.ok){
+        console.log("Failed to fetch data!");
+        return null;
+    }
+
+    return request.json();
+}).then((fetchedData) => {
+    console.log(fetchedData);
+    fetchedData.forEach((item, index) => {
+        titles[index].innerHTML = item.title;
+        currentTimes[index].innerHTML = `${item.timeframes.weekly.current}hrs`;
+        previousTimes[index].innerHTML = `Last Week - ${item.timeframes.weekly.previous}hrs`;
+    });
+    data = fetchedData;
+});
+
+const update = (timeframe) => {
+    data.forEach((item, index) => {
+        currentTimes[index].innerHTML = `${item.timeframes[timeframe.toLowerCase()].current}hrs`;
+        previousTimes[index].innerHTML = `${perviousText[timeframe.toLowerCase()]} - ${item.timeframes[timeframe.toLowerCase()].previous}hrs`;
+    });
+}
+
+const deactivateItems = () => {
+    listItems.forEach(item => {
+        item.classList.remove("active");
+    });
+}
+
+
+listItems.forEach((item) => {
+    item.addEventListener("click", e => {
+        deactivateItems();
+        e.target.classList.add("active");
+        update(e.target.innerHTML);
+    })
+});
